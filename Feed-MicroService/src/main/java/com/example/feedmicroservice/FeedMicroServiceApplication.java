a4894ef39265d9aef9a649e20b08d916238da77f
ramo6627 <ramodev6627@gmail.com>
2024-01-06 20:38:25 +0100

    cors config.

--- Feed-MicroService/src/main/java/com/example/feedmicroservice/FeedMicroServiceApplication.java
+++ Feed-MicroService/src/main/java/com/example/feedmicroservice/FeedMicroServiceApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @EnableDiscoveryClient
@@ -28,6 +31,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedMicroServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FeedMicroServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 	@GetMapping("/hello")
---
+++ Gateway-Service/src/main/java/com/example/gatewayservice/CorsConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.gatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfiguration extends org.springframework.web.cors.CorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowCredentials(false);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        corsConfiguration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
